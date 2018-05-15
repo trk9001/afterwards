33d20bb8c0ce8e285b1845542d9f8522e9157326
trk <dev.trk.9001@gmail.com>
2018-05-16 00:35:56 +0600

    Close #2; add support for custom input files
    Also made minor changes to improve modularity, including adding comments and restructuring code for readability.

--- trkopx.py
+++ trkopx.py
@@ -1,3 +1,5 @@
+#! venv/bin/python
+
 """Script to help fill out product descriptions for Stand-Out.net.
 
 Under development. Since all the necessary methods are encapsulated in a class,
@@ -12,12 +14,12 @@ Usage:
 > Invoke beta() without or with the number of rows as a parameter.
 > Pay attention to and handle any raised exceptions.
 
-```
+---example use---
 from trkopx import FillSheet
-o = FillSheet(69)
+o = FillSheet()
 o.alpha()
 o.beta()
-```
+
 """
 
 import argparse
@@ -30,187 +32,214 @@ from openpyxl.styles import Alignment, Font
 
 class FillSheet:
 
-    SHEET = 'Descr.xlsx'
+    """
+    Column labels in the alpha & beta methods:
 
-    def __init__(self, rows=None):
-        if not os.path.exists(self.SHEET):
-            raise FileNotFoundError
+    D1: Description 1
+    D2: Description 2
+    M: Manufacturer
+    P: Product
+    C: Colour
 
-        if rows is None or isinstance(rows, int):
-            self.rows = rows
-        else:
-            raise TypeError('Invalid type for rows in constructor')
+    """
 
-        self.seed = self.get_mc()
-        if self.seed is None:
-            raise TypeError('Not seeded from file')
+    DEFAULT_SHEET = 'Descr.xlsx'
 
-        self.a = self.alpha
-        self.b = self.beta
+    def __init__(self, sheet=None, rows=None):
 
-    @classmethod
-    def get_mc(cls):
-        """Return the number of the Manufacturer column"""
+            # Attributes
+            self.SHEET = None
+            self.rows = None
+            self.seed = None
 
-        wb = load_workbook(cls.SHEET)
-        ws = wb.active
+            self.SHEET = sheet if sheet else self.DEFAULT_SHEET
 
-        for col in range(2, 10):
-            if ws.cell(row=1, column=col).value == 'MANUFACTURER':
-                return col
+            if not os.path.exists(self.SHEET):
+                raise FileNotFoundError
 
-    @staticmethod
-    def format_cell(cell):
-        cell.alignment = Alignment(horizontal='left', wrap_text=True)
-        cell.font = Font(name='Calibri', size=8)
+            if rows is None:
+                self.rows = self.get_rows()
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Column labels in the alpha & beta methods:
-    #
-    # D1: Description 1
-    # D2: Description 2
-    # M: Manufacturer
-    # P: Product
-    # C: Colour
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            elif isinstance(rows, int):
+                self.rows = rows
+
+            else:
+                raise TypeError('Invalid type for rows in constructor')
+
+            seed = self.get_mc()
+
+            if seed:
+                self.seed = seed
+
+            else:
+                raise TypeError('Not seeded from file')
+
+            # Aliases
+            self.a = self.alpha
+            self.b = self.beta
+
+    def get_rows(self):
+            """Return the number of rows in the sheet"""
+
+            wb = load_workbook(self.SHEET)
+            return len(list(wb.active.rows))
+
+    def get_mc(self):
+            """Return the index of the Manufacturer column"""
+
+            wb = load_workbook(self.SHEET)
+            ws = wb.active
+
+            for col in range(2, 10):
+                if ws.cell(row=1, column=col).value == 'MANUFACTURER':
+                    return col
 
     def alpha(self, rows=None):
-        if rows is not None:
-            if isinstance(rows, int):
-                self.rows = rows
-            else:
-                raise TypeError('Invalid type for rows in alpha')
 
-        if self.rows is None:
-            raise TypeError('Invalid type for self.rows in alpha')
-
-        # Set column numbers sequentially from seed value
-        M, P, C, D1, D2 = (self.seed + i for i in range(5))
-
-        wb = load_workbook(self.SHEET)
-        ws = wb.active
-
-        p = ''
-
-        i = 1
-        while i < self.rows:
-            i += 1
-
-            if i > 2 and ws.cell(row=i, column=P).value == p:
-                continue
-
-            m = ws.cell(row=i, column=M).value
-            p = ws.cell(row=i, column=P).value
-            c = ws.cell(row=i, column=C).value
-
-            descr = f'The {p} from {m} comes in {c} colour, featuring'
-            # TODO: Refine to accommodate plural products
-
-            self.format_cell(ws.cell(row=i, column=D2))
-            ws.cell(row=i, column=D2).value = descr
-
-        wb.save(self.SHEET)
-
-    def beta(self, rows=None):
-        if rows is not None:
-            if isinstance(rows, int):
-                self.rows = rows
-            else:
-                raise TypeError('Invalid type for rows in beta')
-
-        if self.rows is None:
-            raise TypeError('Invalid type for self.rows in beta')
-
-        # Set column numbers sequentially from seed value
-        M, P, C, D1, D2 = (self.seed + i for i in range(5))
-
-        wb = load_workbook(self.SHEET)
-        ws = wb.active
-
-        m = ''
-        p = ''
-        c = ''
-
-        i = 1
-        times_repeated = 0
-        while i < self.rows:
-            i += 1
-
-            # Manual skip condition (either cell works)
-            if (ws.cell(row=i, column=D1).value == 'SKIP'
-                    or ws.cell(row=i, column=D2).value == 'SKIP'):
-                continue
-
-            if ws.cell(row=i, column=P).value == p:  # Last p (not updated yet)
-                if times_repeated == 0 or times_repeated == 2:
-                    new_c = ws.cell(row=i, column=C).value
-
-                    new_D1 = ws.cell(row=i-1, column=D2).value
-                    new_D1 = new_D1.replace(c, new_c)
-
-                    new_D2 = ws.cell(row=i-1, column=D1).value
-                    new_D2 = new_D2.replace(c, new_c)
-
-                elif times_repeated == 1:
-                    new_c = ws.cell(row=i, column=C).value
-
-                    elder = f'From {m} comes'
-                    youngster = f'{m} offers'
-                    new_D1 = ws.cell(row=i-2, column=D1).value
-                    new_D1 = new_D1.replace(elder, youngster).replace(c, new_c)
-
-                    elder = f'The {p} from {m}'
-                    youngster = f'Offered by {m}, the {p}'
-                    new_D2 = ws.cell(row=i-2, column=D2).value
-                    new_D2 = new_D2.replace(elder, youngster).replace(c, new_c)
-
+            if rows is not None:
+                if isinstance(rows, int):
+                    self.rows = rows
                 else:
+                    raise TypeError('Invalid type for rows in alpha')
+
+            if self.rows is None:
+                raise TypeError('Invalid type for self.rows in alpha')
+
+            # Set column numbers sequentially from seed value
+            M, P, C, D1, D2 = (self.seed + i for i in range(5))
+
+            wb = load_workbook(self.SHEET)
+            ws = wb.active
+
+            p = ''
+
+            i = 1
+            while i < self.rows:
+                i += 1
+
+                if i > 2 and ws.cell(row=i, column=P).value == p:
                     continue
-
-                self.format_cell(ws.cell(row=i, column=D1))
-                ws.cell(row=i, column=D1).value = new_D1
-
-                self.format_cell(ws.cell(row=i, column=D2))
-                ws.cell(row=i, column=D2).value = new_D2
-
-                times_repeated += 1
-
-            else:
-                times_repeated = 0
-                full_descr = True
-
-                if ws.cell(row=i, column=D1).value == 'EZPZ':
-                    full_descr = False
 
                 m = ws.cell(row=i, column=M).value
                 p = ws.cell(row=i, column=P).value
                 c = ws.cell(row=i, column=C).value
 
-                D2_val = ws.cell(row=i, column=D2).value
-                start_ = f'From {m} comes the {p} in {c} colour, '
-                end_ = ''
-                try:
-                    end_ = re.search(r'featuring.*$', D2_val).group(0)
-                except Exception as e:
-                    print(i, type(e), e)
+                descr = f'The {p} from {m} comes in {c} colour, featuring'
                 # TODO: Refine to accommodate plural products
 
-                descr = start_ + end_
-                if full_descr:
+                self.format_cell(ws.cell(row=i, column=D2))
+                ws.cell(row=i, column=D2).value = descr
+
+            wb.save(self.SHEET)
+
+    def beta(self, rows=None):
+
+            if rows is not None:
+                if isinstance(rows, int):
+                    self.rows = rows
+                else:
+                    raise TypeError('Invalid type for rows in beta')
+
+            if self.rows is None:
+                raise TypeError('Invalid type for self.rows in beta')
+
+            # Set column numbers sequentially from seed value
+            M, P, C, D1, D2 = (self.seed + i for i in range(5))
+
+            wb = load_workbook(self.SHEET)
+            ws = wb.active
+
+            m = ''
+            p = ''
+            c = ''
+
+            i = 1
+            times_repeated = 0
+            while i < self.rows:
+                i += 1
+
+                # Manual skip condition (either cell works)
+                if (ws.cell(row=i, column=D1).value == 'SKIP'
+                        or ws.cell(row=i, column=D2).value == 'SKIP'):
+                    continue
+
+                if ws.cell(row=i, column=P).value == p:  # Last p (not updated yet)
+                    if times_repeated == 0 or times_repeated == 2:
+                        new_c = ws.cell(row=i, column=C).value
+
+                        new_D1 = ws.cell(row=i-1, column=D2).value
+                        new_D1 = new_D1.replace(c, new_c)
+
+                        new_D2 = ws.cell(row=i-1, column=D1).value
+                        new_D2 = new_D2.replace(c, new_c)
+
+                    elif times_repeated == 1:
+                        new_c = ws.cell(row=i, column=C).value
+
+                        elder = f'From {m} comes'
+                        youngster = f'{m} offers'
+                        new_D1 = ws.cell(row=i-2, column=D1).value
+                        new_D1 = new_D1.replace(elder, youngster).replace(c, new_c)
+
+                        elder = f'The {p} from {m}'
+                        youngster = f'Offered by {m}, the {p}'
+                        new_D2 = ws.cell(row=i-2, column=D2).value
+                        new_D2 = new_D2.replace(elder, youngster).replace(c, new_c)
+
+                    else:
+                        continue
+
+                    self.format_cell(ws.cell(row=i, column=D1))
+                    ws.cell(row=i, column=D1).value = new_D1
+
+                    self.format_cell(ws.cell(row=i, column=D2))
+                    ws.cell(row=i, column=D2).value = new_D2
+
+                    times_repeated += 1
+
+                else:
+                    times_repeated = 0
+                    full_descr = True
+
+                    if ws.cell(row=i, column=D1).value == 'EZPZ':
+                        full_descr = False
+
+                    m = ws.cell(row=i, column=M).value
+                    p = ws.cell(row=i, column=P).value
+                    c = ws.cell(row=i, column=C).value
+
+                    D2_val = ws.cell(row=i, column=D2).value
+                    start_ = f'From {m} comes the {p} in {c} colour, '
+                    end_ = ''
                     try:
-                        ft = re.search(r'featuring ([^.]*)[.]', descr).group(1)
-                        sport = re.search(r'sports? (.*)[.]$', descr).group(1)
+                        end_ = re.search(r'featuring.*$', D2_val).group(0)
                     except Exception as e:
                         print(i, type(e), e)
+                    # TODO: Refine to accommodate plural products
 
-                    descr = descr.replace(sport, '#')
-                    descr = descr.replace(ft, sport)
-                    descr = descr.replace('#', ft)
+                    descr = start_ + end_
+                    if full_descr:
+                        try:
+                            ft = re.search(r'featuring ([^.]*)[.]', descr).group(1)
+                            sport = re.search(r'sports? (.*)[.]$', descr).group(1)
+                        except Exception as e:
+                            print(i, type(e), e)
 
-                self.format_cell(ws.cell(row=i, column=D1))
-                ws.cell(row=i, column=D1).value = descr
+                        descr = descr.replace(sport, '#')
+                        descr = descr.replace(ft, sport)
+                        descr = descr.replace('#', ft)
 
-        wb.save(self.SHEET)
+                    self.format_cell(ws.cell(row=i, column=D1))
+                    ws.cell(row=i, column=D1).value = descr
+
+            wb.save(self.SHEET)
+
+    @staticmethod
+    def format_cell(cell):
+            """Visually format one worksheet cell as specified"""
+
+            cell.alignment = Alignment(horizontal='left', wrap_text=True)
+            cell.font = Font(name='Calibri', size=8)
 
 # End of FillSheet
 
