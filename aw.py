1ac73d72b529a93b740693741c96fda1a07e9d73
trk <dev.trk.9001@gmail.com>
2018-07-09 16:08:15 +0600

    Add a date parser and documentation
    Also refactored code, cleaned up obsolete functions, and put the main stuff in main().

--- aw.py
+++ aw.py
@@ -1,51 +1,97 @@
 #!/usr/bin/env python3
 
-"""./aw.py 07/08 1350 notify-send Hello"""
+"""Afterwards (aw).
 
-import sys  # for command line args
-import subprocess as sp  # for system calls
-from textwrap import dedent  # for proper multiline string indentation
+See the README in docs/ or
+https://github.com/trk9001/afterwards
+
+This is in development. Intended usage:
+<aw date time [-r minutes] text>
+
+"""
+
+import re
+import sys
+from subprocess import call
+from textwrap import dedent
 
 
-def month(n):
-    """Return month name for the number"""
-    if not isinstance(n, int):
-        n = int(n)
+_arg_i = 0  # Global index for sys.argv
 
-    months = {
-        1: 'January',
-        2: 'February',
-        3: 'March',
-        4: 'April',
-        5: 'May',
-        6: 'June',
-        7: 'July',
-        8: 'August',
-        9: 'September',
-        10: 'October',
-        11: 'November',
-        12: 'December'
-    }
 
-    return months[n];
+def parse_date(dt):
+    """A date-parser function.
+
+    Converts a date's format from various possible ones to a single convenient
+    one, and returns it.
+
+    Args:
+        dt: The date argument as read from the command line.
+
+    """
+
+    months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    if dt in ['today', 'tomorrow']:
+        pass
+
+    elif dt in months or dt in [m[:3] for m in months]:
+        global _arg_i
+        dt = ' '.join([dt, str(int(sys.argv[_arg_i + 1]))])
+        _arg_i += 1
+
+    elif re.fullmatch(r'\d{1,2}[./-]\d{1,2}', dt):
+        for sep in './-':
+            if sep in dt:
+                dt = dt.split(sep)
+                break
+
+        month_chart = dict()
+        for i, month in enumerate(months):
+            month_chart[i + 1] = month
+
+        dt = ' '.join([month_chart[int(dt[0])], str(int(dt[1]))])
+    
+    else:
+        return None
+
+    return dt
+
+
+def main():
+    """Main function."""
+
+    _arg_i += 1
+    aw_date = sys.argv[_arg_i]
+    aw_date = parse_date(aw_date)
+
+    _arg_i += 1
+    aw_time = sys.argv[_arg_i]
+
+    _arg_i += 1
+    aw_text = ' '.join(sys.argv[_arg_i:])
+
+    cmd = '''\
+    at {} {} << _AW_
+    notify-send "{}"
+    _AW_'''
+
+    cmd = dedent(cmd.format(aw_time, aw_date, aw_text))
+    call(cmd, shell=True)
 
 
 if __name__ == '__main__':
-    tdate = sys.argv[1]
-    tdate = tdate.split(tdate[2])
-    tdate = [month(tdate[0]), str(int(tdate[1]))]
-    tdate = ' '.join(tdate)
-
-    ttime = list(sys.argv[2])
-    ttime.insert(2, ':')
-    ttime = ''.join(ttime)
-
-    ttext = ' '.join(sys.argv[3:])
-
-    cmd = '''\
-    at {} {} << MARK
-    {}
-    MARK'''
-
-    cmd = dedent(cmd.format(ttime, tdate, ttext))
-    sp.call(cmd, shell=True)
+    main()
