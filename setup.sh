2c70b830d9ef6c3b2e5e125ff711eaa7033c76b2
trk <dev.trk.9001@gmail.com>
2018-07-12 13:44:47 +0600

    Automate more setup tasks

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.aw
-cp sound/alarm.mp3 ~/.aw/
+mkdir ~/.aw
+mkdir ~/.aw/docs ~/.aw/sounds ~/.aw/resources ~/.aw/messages ~/.aw/bin
+
 cp -r docs ~/.aw/
+cp sounds/alarm.mp3 ~/.aw/sounds
+cp resources/msg-template ~/.aw/resources/
+cp -r bin ~/.aw/
+
+chmod +x ~/.aw/bin/*
 
 if [[ ! -d "~/bin" ]]; then
-    mkdir -p ~/bin
+    mkdir ~/bin
 fi
-cp aw.py ~/bin/
+
+ln -s ~/.aw/bin/aw.py ~/bin/aw
 
 exit 0
