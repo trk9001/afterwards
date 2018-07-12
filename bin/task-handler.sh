c20a528e0e5ba3171335008849551fd1386e33c4
trk <dev.trk.9001@gmail.com>
2018-07-12 14:39:48 +0600

    Set the DISPLAY environment variable for zenity
    The window refused to display. Solution:
    https://unix.stackexchange.com/a/215017

--- bin/task-handler.sh
+++ bin/task-handler.sh
@@ -2,12 +2,12 @@
 
 msgFile="$1"
 
-(sleep 0.6; play -q ~/aw/sounds/alarm.mp3 > /dev/null 2>&1) &
+(sleep 0.6; play -q ~/aw/sounds/alarm.mp3) &
 
+env DISPLAY=:0.0 \
 zenity --text-info \
        --title="Reminder" \
        --filename="$msgFile" \
-       --html --width=500 --height=175 \
-       > /dev/null 2>&1
+       --html --width=500 --height=175
 
 killall play
