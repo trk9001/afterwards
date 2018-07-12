1a27773963399294c424197655db8dc89b66132f
trk <dev.trk.9001@gmail.com>
2018-07-12 13:46:36 +0600

    Finalize alert creation process

--- resources/task-handler.sh
+++ resources/task-handler.sh
@@ -2,10 +2,12 @@
 
 msgFile="$1"
 
+(sleep 0.6; play -q ~/aw/sounds/alarm.mp3 > /dev/null 2>&1) &
+
 zenity --text-info \
        --title="Reminder" \
-       --filename=$msgFile \
+       --filename="$msgFile" \
        --html --width=500 --height=175 \
        > /dev/null 2>&1
 
-play ~/.aw/sounds/alarm.mp3 trim 0 3
+killall play
