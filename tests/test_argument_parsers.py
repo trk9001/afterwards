c4295c5f10c9f8c94460a0d857368d6c242ed4f5
trk <dev.trk.9001@gmail.com>
2018-08-22 16:41:55 +0600

    Finalize test suite for the argument parser

--- tests/test_argument_parsers.py
+++ tests/test_argument_parsers.py
@@ -1,54 +1,54 @@
 from aw.afterwards import AwArgumentParser
-from .context import SysOverwrite
-
-
-sys = SysOverwrite()
-parser = AwArgumentParser()
 
 
 def test_parse_for_date_relative():
-    sys.build_args('today 1337 Eat, code and sleep.')
-    parser.arg_i = 0
+    cmd = 'aw today 1337 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
     assert parser.parse_for_date() == 'today'
 
 
 def test_parse_for_date_words():
-    sys.build_args('Feb 29 1337 Eat, code and sleep.')
-    parser.arg_i = 0
-    assert parser.parse_for_date() == 'Feb 29'
+    cmd = 'aw Feb 29 1337 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
+    assert parser.parse_for_date() == 'feb 29'
 
 
 def test_parse_for_date_numeric():
-    sys.build_args('02/29 1337 Eat, code and sleep.')
-    parser.arg_i = 0
-    assert parser.parse_for_date() == 'February 29'
+    cmd = 'aw 02/29 1337 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
+    assert parser.parse_for_date() == 'february 29'
 
 
 def test_parse_for_date_failure():
-    sys.build_args('LMAO 1337 Eat, code and sleep.')
-    parser.arg_i = 0
+    cmd = 'aw LMAO 1337 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
     assert parser.parse_for_date() is None
 
 
 def test_parse_for_time_24hr():
-    sys.build_args('today 1337 Eat, code and sleep.')
-    parser.arg_i = 1
+    cmd = 'aw today 1337 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
+    parser.parse_for_date()
     assert parser.parse_for_time() == '1337'
 
 
 def test_parse_for_time_12hr():
-    sys.build_args('today 1:37 pm Eat, code and sleep.')
-    parser.arg_i = 1
+    cmd = 'aw today 1:37 pm Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
+    parser.parse_for_date()
     assert parser.parse_for_time() == '1:37 PM'
 
 
 def test_parse_for_time_failure():
-    sys.build_args('today 42 Eat, code and sleep.')
-    parser.arg_i = 1
+    cmd = 'aw today 42 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
+    parser.parse_for_date()
     assert parser.parse_for_time() is None
 
 
 def test_parse_for_msg():
-    sys.build_args('today 1337 Eat, code and sleep.')
-    parser.arg_i = 2
+    cmd = 'aw today 1337 Eat, code and sleep.'
+    parser = AwArgumentParser(cmd.split())
+    parser.parse_for_date()
+    parser.parse_for_time()
     assert parser.parse_for_msg() == 'Eat, code and sleep.'
