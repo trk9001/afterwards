0e07250c8b23ae9f3e8004014443d94c67ecc40c
trk <dev.trk.9001@gmail.com>
2018-08-22 18:52:11 +0600

    Update setup details

--- aw/bin/uninstall.sh
+++ aw/bin/uninstall.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-rm -rf "$HOME/.aw"
+rm -rf "$HOME/.afterwards"
 rm "$HOME/bin/aw"
--- setup.sh
+++ setup.sh
@@ -1,25 +1,22 @@
 #! /usr/bin/env bash
 
-mkdir "$HOME/.aw"
-mkdir "$HOME/.aw/docs" \
-      "$HOME/.aw/sounds" \
-      "$HOME/.aw/resources" \
-      "$HOME/.aw/messages" \
-      "$HOME/.aw/bin"
+mkdir -p "$HOME/.afterwards"
+cp -r aw "$HOME/.afterwards/"
 
-cp -r docs "$HOME/.aw/"
-cp sounds/alarm.mp3 "$HOME/.aw/sounds"
-cp resources/msg-template "$HOME/.aw/resources/"
-cp -r bin "$HOME/.aw/"
+# Remove Python packaging
+rm "$HOME/.afterwards/aw/__init__.py"
 
-chmod +x "$HOME/.aw/bin/aw.py"
-chmod +x "$HOME/.aw/bin/task-handler.sh"
-chmod +x "$HOME/.aw/bin/uninstall.sh"
+# Executable scripts
+chmod +x "$HOME/.afterwards/aw/afterwards.py"
+chmod +x "$HOME/.afterwards/aw/bin/task-handler.sh"
+chmod +x "$HOME/.afterwards/aw/bin/uninstall.sh"
 
+# Installs to user's ~/bin
 if [[ ! -d "$HOME/bin" ]]; then
     mkdir "$HOME/bin"
 fi
 
-ln -s "$HOME/.aw/bin/aw.py" "$HOME/bin/aw"
+# Symlink to the main script in ~/bin
+ln -s "$HOME/.afterwards/aw/afterwards.py" "$HOME/bin/aw"
 
-exit 0
+return 0
